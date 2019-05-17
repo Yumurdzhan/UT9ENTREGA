df6fa5e9ed69ac72ad2876a07ed91b4b6d1200c5
Yumurdzhan <yumurdzhan17@gmail.com>
2019-05-17 09:28:16 +0200

    ProyectoUT9

---
+++ src/vistacontrolador/GuiCorrectorProyectos.java
@@ -0,0 +1,192 @@
+//  Autor: Yumurdzhan
+package vistacontrolador;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import modelo.CorrectorProyectos;
+
+public class GuiCorrectorProyectos extends Application
+{
+
+	private MenuItem itemLeer;
+	private MenuItem itemGuardar;
+	private MenuItem itemSalir;
+
+	private TextField txtAlumno;
+	private Button btnVerProyecto;
+
+	private RadioButton rbtAprobados;
+	private RadioButton rbtOrdenados;
+	private Button btnMostrar;
+
+	private TextArea areaTexto;
+
+	private Button btnClear;
+	private Button btnSalir;
+
+	private CorrectorProyectos corrector; // el modelo
+
+	@Override
+	public void start(Stage stage) {
+
+		corrector = new CorrectorProyectos();
+
+		BorderPane root = crearGui();
+
+		Scene scene = new Scene(root, 800, 600);
+		stage.setScene(scene);
+		stage.setTitle("- Corrector de proyectos -");
+		scene.getStylesheets().add(getClass()
+		                .getResource("/css/application.css").toExternalForm());
+		stage.show();
+	}
+
+	private BorderPane crearGui() {
+
+		BorderPane panel = new BorderPane();
+		MenuBar barraMenu = crearBarraMenu();
+		panel.setTop(barraMenu);
+
+		VBox panelPrincipal = crearPanelPrincipal();
+		panel.setCenter(panelPrincipal);
+
+		HBox panelBotones = crearPanelBotones();
+		panel.setBottom(panelBotones);
+
+		return panel;
+	}
+
+	private MenuBar crearBarraMenu() {
+
+		MenuBar barraMenu = new MenuBar();
+
+		Menu menu = new Menu("Archivo");
+
+		itemLeer = new MenuItem("_Leer de fichero");
+		itemLeer.setAccelerator(KeyCombination.keyCombination("CTRL+L"));
+		// a completar
+
+
+
+
+		return barraMenu;
+	}
+
+	private VBox crearPanelPrincipal() {
+
+		VBox panel = new VBox();
+		panel.setPadding(new Insets(5));
+		panel.setSpacing(10);
+
+		Label lblEntrada = new Label("Panel de entrada");
+		// a completar
+
+
+
+
+		return panel;
+	}
+
+	private HBox crearPanelEntrada() {
+
+		HBox panel = new HBox();
+		panel.setPadding(new Insets(5));
+		// a completar
+
+
+
+
+		return panel;
+	}
+
+	private HBox crearPanelOpciones() {
+
+		HBox panel = new HBox();
+		panel.setPadding(new Insets(5));
+		// a completar
+
+
+
+		return panel;
+	}
+
+	private HBox crearPanelBotones() {
+
+		HBox panel = new HBox();
+		panel.setPadding(new Insets(5));
+		// a completar
+
+
+
+
+		return panel;
+	}
+
+	private void salvarEnFichero() {
+		// a completar
+
+
+
+
+	}
+
+	private void leerDeFichero() {
+		// a completar
+
+
+
+
+	}
+
+	private void verProyecto() {
+		// a completar
+
+	}
+
+	private void mostrar() {
+
+		clear();
+		// a completar
+
+
+
+
+	}
+
+	private void cogerFoco() {
+
+		txtAlumno.requestFocus();
+		txtAlumno.selectAll();
+
+	}
+
+	private void salir() {
+
+		System.exit(0);
+	}
+
+	private void clear() {
+
+		areaTexto.clear();
+		cogerFoco();
+	}
+
+	public static void main(String[] args) {
+
+		launch(args);
+	}
+}
